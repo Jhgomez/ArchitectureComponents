47de23dccef19fc0d420cd4feb69f0990017200c
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-10 10:34:53 -0600

    chore: add property

--- app/src/main/java/com/example/dessertclicker/DessertUiState.kt
+++ app/src/main/java/com/example/dessertclicker/DessertUiState.kt
@@ -4,5 +4,6 @@ import com.example.dessertclicker.model.Dessert
 
 data class DessertUiState (
     val desserts: List<Dessert> = emptyList(),
-    val revenue: Int = 0
+    val revenue: Int = 0,
+    val dessertsSold: Int = 0
 )
