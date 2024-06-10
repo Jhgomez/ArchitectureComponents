820596afe3dc258bd51e0b3e22947ce8c84c16f3
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-10 11:07:23 -0600

    chore: pass list

--- app/src/main/java/com/example/dessertclicker/DessertViewModel.kt
+++ app/src/main/java/com/example/dessertclicker/DessertViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.dessertclicker
 
 import androidx.lifecycle.ViewModel
+import com.example.dessertclicker.data.Datasource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
@@ -8,5 +9,9 @@ class DessertViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(DessertUiState())
     val uiState = _uiState.asStateFlow()
 
-
+    init {
+        _uiState.value = DessertUiState(
+            desserts = Datasource.dessertList
+        )
+    }
 }
