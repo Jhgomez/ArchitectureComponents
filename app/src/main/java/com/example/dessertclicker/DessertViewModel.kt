6704ab4f3ae557a9fbc79afa02279d7ec3e6bbfc
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-10 10:47:20 -0600

    chore: hold state

--- app/src/main/java/com/example/dessertclicker/DessertViewModel.kt
+++ app/src/main/java/com/example/dessertclicker/DessertViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.dessertclicker
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class DessertViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow(DessertUiState())
+    val uiState = _uiState.asStateFlow()
+
+
 }
