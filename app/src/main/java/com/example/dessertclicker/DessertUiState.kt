71eea5e2bdcf41f5caf9f96fce9a3567ef19519c
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-10 11:41:49 -0600

    chore: add property

--- app/src/main/java/com/example/dessertclicker/DessertUiState.kt
+++ app/src/main/java/com/example/dessertclicker/DessertUiState.kt
@@ -8,5 +8,5 @@ data class DessertUiState (
     val dessertsSold: Int = 0,
     val currentDessertIndex: Int = 0,
     val currentDessertPrice: Int = 0,
-    val currentDessertImageId
+    val currentDessertImageId: Int = 0
 )
