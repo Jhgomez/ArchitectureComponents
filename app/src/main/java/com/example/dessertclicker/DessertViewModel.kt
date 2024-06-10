e3b288451a34284b6829a13dd03acfc030fd1ea7
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-10 11:54:30 -0600

    chore: add fun

--- app/src/main/java/com/example/dessertclicker/DessertViewModel.kt
+++ app/src/main/java/com/example/dessertclicker/DessertViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.dessertclicker.data.Datasource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class DessertViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(DessertUiState())
@@ -20,5 +21,19 @@ class DessertViewModel: ViewModel() {
         )
     }
 
-//    fun addToCart(int)
+    fun addItemToCart() {
+        _uiState.update { currentStatus ->
+            val revenue = currentStatus.revenue + currentStatus.currentDessertPrice
+            val dessertsSold = currentStatus.dessertsSold.inc()
+            // Show the next dessert
+            val dessertToShow = determineDessertToShow(currentStatus.desserts, dessertsSold)
+
+            currentStatus.copy(
+                dessertsSold = dessertsSold,
+                currentDessertPrice = dessertToShow.price,
+                currentDessertImageId = dessertToShow.imageId,
+                revenue = revenue
+            )
+        }
+    }
 }
