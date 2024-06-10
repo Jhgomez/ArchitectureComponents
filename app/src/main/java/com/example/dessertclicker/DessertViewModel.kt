e67d77c6feb2b1704bcdeba06c2e0d28fb0d561a
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-10 11:43:45 -0600

    chore: pass initial values

--- app/src/main/java/com/example/dessertclicker/DessertViewModel.kt
+++ app/src/main/java/com/example/dessertclicker/DessertViewModel.kt
@@ -9,9 +9,16 @@ class DessertViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(DessertUiState())
     val uiState = _uiState.asStateFlow()
 
+//    private val currentDessertPrice =
+
     init {
         _uiState.value = DessertUiState(
-            desserts = Datasource.dessertList
+            desserts = Datasource.dessertList,
+            currentDessertImageId = Datasource.dessertList[0].imageId,
+            currentDessertPrice = Datasource.dessertList[0].imageId
+
         )
     }
+
+//    fun addToCart(int)
 }
